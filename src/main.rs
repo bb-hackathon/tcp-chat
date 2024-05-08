5c7804deb6a6a859cde862148edcef6afdc6d109
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-05-08 20:41:59 +0300

    feat: Add some `actix` boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Le TCP chat.");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    let _ = color_eyre::install();
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8082))?
+    .run()
+    .await
 }
