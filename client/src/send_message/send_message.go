3230c0099c9555fdd43a73a79743292eb3a2b655
sjewyr <ganshin.2004@gmail.com>
2024-05-22 13:28:56 +0300

    feat: basic message(text) handling on client

--- client/src/send_message/send_message.go
+++ client/src/send_message/send_message.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
@@ -10,7 +13,32 @@ import (
 	"bb-hackathon/tcp-chat.git/proto"
 )
 
-func main() {
+func CORSHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+type Message struct {
+	Message string `json:"message"`
+}
+
+func sendMessageHandler(w http.ResponseWriter, r *http.Request) {
+	var text Message
+	err := json.NewDecoder(r.Body).Decode(&text)
+	t := text.Message
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 	conn, err := grpc.Dial("luna:9001", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
@@ -28,13 +56,28 @@ func main() {
 
 	messageReq := &proto.ClientsideMessage{
 		RoomUuid: &proto.UUID{Uuid: "36537ef0-ee3e-44fa-aaa1-6547e466dd4a"},
-		Text:     "Hello, BOBR!",
+		Text:     t,
 	}
 
 	_, err = client.SendMessage(ctx, messageReq)
 	if err != nil {
 		log.Fatalf("SendMessage failed: %v", err)
 	} else {
-		log.Println("Message sent successfully")
+		log.Println("Message sent successfully: %s", text)
 	}
+
+	// Ответ клиенту
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "success"})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/send", sendMessageHandler)
+
+	// Добавляем CORSHandler
+	handler := CORSHandler(mux)
+	http.HandleFunc("/send", sendMessageHandler)
+	fmt.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
